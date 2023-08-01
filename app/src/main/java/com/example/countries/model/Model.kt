90bf39a04eabd81a1c30db9f47d6e0f3e7e4d88c
semih <semih_bas_1905@outlook.com>
2023-08-01 12:34:40 +0300

    Model++

---
+++ app/src/main/java/com/example/countries/model/Model.kt
@@ -0,0 +1,10 @@
+package com.example.countries.model
+
+
+data class Counrty(
+    val countryName:String?,
+    val countryRegion:String?,
+    val countryCapital:String?,
+    val countryCurrency:String?,
+    val capitalLanguage:String?,
+    val imageUrl:String?)
